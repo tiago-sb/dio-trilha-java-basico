bcda73a1220c96661b14c80947bb82b9bd2e754d
tiago-sb <belatiago72@gmail.com>
2023-09-13 07:37:14 -0300

    adiciona mudanças para a pasta tipos-variaveis/ no que se refere as linhas de código da classe

--- tipos-variaveis/src/APP.java
+++ tipos-variaveis/src/APP.java
@@ -1,9 +1,12 @@
 
 public class APP {
-
 	public static void main(String[] args) {
-		System.out.print("Ola, mundo!");
-
+		System.out.print("Ola, mundo! ");
+		final double PI = 3.14;
+		byte idade = 13; // byte é o tipo numérico de menor espaço na memória
+		System.out.print("tenho " + idade + " anos!");
+		short ano = 2023; // short é o segundo tipo com menor espaço
+		System.out.print("e estamos no ano " + ano );
+		int quantidade = 2345; 
 	}
-
 }
