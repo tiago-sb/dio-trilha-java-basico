2b2fa48845f97f9f926230cd911cd2fa5726067e
tiago-sb <belatiago72@gmail.com>
2023-09-15 15:19:36 -0300

    adiciona a pasta java-terminal/ ao repositorio remoto

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class AboutMe {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+		//nome
+		System.out.print("seu nome: ");
+		String nome = entrada.nextLine();
+		//sobrenome
+		System.out.print("seu sobrenome: ");
+		String sobrenome = entrada.nextLine();
+		//idade
+		System.out.print("sua idade: ");
+		int idade = entrada.nextInt();
+		//altura
+		System.out.print("sua altura: ");
+		double altura = entrada.nextDouble();
+		//print
+		System.out.println("Me chamo " + nome + " " + sobrenome);
+		System.out.print("tenho " + idade + " anos ");
+		System.out.print("e " + altura + "m de altura");
+		entrada.close();
+	}
+}
---
+++ java-terminal/src/Java_terminal.java
@@ -0,0 +1,5 @@
+public class Java_terminal {
+	public static void main(String[] args) {
+		System.out.println("Ola, mundo! Fui executado pelo cmd!");
+	}
+}
