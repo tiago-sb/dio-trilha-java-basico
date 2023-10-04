f2e12e27a8dc60f7891a1d2682aeb383a767ff20
tiago-sb <belatiago72@gmail.com>
2023-10-04 07:56:09 -0300

    adiciona modulo de enuns

--- java_poo/src/Escola/Escola.java
+++ java_poo/src/Escola/Escola.java
@@ -1,10 +1,11 @@
 package Escola;
 public class Escola {
 	public static void main(String[] args) {
-		Aluno primeiroAluno = new Aluno();
+		Aluno primeiroAluno = new Aluno("tiago","123");
 		
 		primeiroAluno.setIdade(19);
 		primeiroAluno.setAltura(1.78f);
+		
 		System.out.println("Nome: " + primeiroAluno.getNome() + "\nIdade: " + primeiroAluno.getIdade());
 		System.out.println("altura: " + primeiroAluno.getAltura() + "\nCpf: " + primeiroAluno.getCpf());
 	}
---
+++ java_poo/src/enuns/EstadoBrasileiro.java
@@ -0,0 +1,5 @@
+package enuns;
+
+public enum EstadoBrasileiro {
+
+}
